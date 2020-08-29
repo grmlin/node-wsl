be9f978bdde87a899abf551020e1d61ff9d3bc57
Andreas Wehr <aw@decaf.de>
2020-08-29 23:59:15 +0200

    buil: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    // Do not activate notifications. Tests running inside WSL will freeze in the end
-    notify: false,
-    // CoverageReporters: ['json', 'lcov', 'text', 'clover', 'text-summary'],
-    coverageDirectory: '<rootDir>/coverage',
-    modulePathIgnorePatterns: ['<rootDir>/test.old'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // Do not activate notifications. Tests running inside WSL will freeze in the end
+  notify: false,
+  // CoverageReporters: ['json', 'lcov', 'text', 'clover', 'text-summary'],
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: ['<rootDir>/source/**/*.ts'],
 };
